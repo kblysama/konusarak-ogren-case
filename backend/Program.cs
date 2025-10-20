c5050cd0c032213e97a69e1be367660a14b9186c
Kubilay Sama <kblysama@gmail.com>
2025-10-20 13:04:09 +0300

    refactor test

--- backend/Program.cs
+++ backend/Program.cs
@@ -45,13 +45,7 @@ app.MapPost("/register", async (ChatDb db, User user) =>
     return Results.Ok(new { user.Id, user.Nickname });
 });
 
-app.MapGet("/messages", async (ChatDb db) =>
-{
-    var list = await db.Messages
-        .OrderBy(m => m.CreatedAt)
-        .ToListAsync();
-    return Results.Ok(list);
-});
+
 
 app.MapPost("/message", async (ChatDb db, IHttpClientFactory httpFactory, MessageIn payload, IConfiguration cfg) =>
 {
